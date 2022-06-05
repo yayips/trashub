913434f3dd4a4ddfe3cdccf0ae025f74ddb43a34
yayips <99575720+yayips@users.noreply.github.com>
2022-06-05 15:59:03 +0700

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -1,14 +1,22 @@
 const mysql = require("mysql2");
+  
+const db_connection = async config => {   // [START cloud_sql_mysql_mysql_create_socket]
+  const dbSocketPath = process.env./cloudsql/test_net || '/cloudsql';
 
-const db_connection = mysql
-  .createConnection({
-    host: "localhost", // HOST NAME
-    user: "root", // USER NAME
-    database: "trushhub", // DATABASE NAME
-    password: "", // DATABASE PASSWORD
-  })
+  // Establish a connection to the database
+  return mysql.createPool({
+    user: process.env.root, // e.g. 'my-db-user'
+    password: process.env.12345*, // e.g. 'my-db-password'
+    database: process.env.banksampah, // e.g. 'my-database'
+    // If connecting via unix domain socket, specify the path
+    socketPath: `${dbSocketPath}/${process.env.test_net}`,
+    // Specify additional properties here.
+    ...config,
+  });
   .on("error", (err) => {
     console.log("Failed to connect to Database - ", err);
-  });
+};
+// [END cloud_sql_mysql_mysql_create_socket]
+
 
 module.exports = db_connection;
